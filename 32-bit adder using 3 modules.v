7fe6dbedd5d9a725b59bc825a3545bcc332f990f
Ayush Nimkar <ayushmn29@gmail.com>
2026-07-01 20:04:19 +0530

    32 bit adder using 16 bit adder which is designed using instantiations of 1 bit adders.

---
+++ 32-bit adder using 3 modules.v
@@ -0,0 +1,21 @@
+module top_module (
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum,
+    input cin,
+    output cout
+);
+    add16 inst1 ( .a(a[15:0]), .b(b[15:0]), .sum(sum[15:0]), .cin(0), .cout(cout));
+    add16 inst2 ( .a(a[31:16]), .b(b[31:16]), .sum(sum[31:16]), .cin(cout),.cout(0));
+
+endmodule
+
+module add1 ( input a, input b, input cin,   output sum, output cout );
+    
+    assign sum = a ^ b ^ cin;
+    assign cout = (a&b)|(cin&(a^b));
+endmodule
+    
+        
+
+
