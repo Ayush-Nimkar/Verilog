596b0dc7c5db5a3d84833e0cc63640f884cbca3b
Ayush Nimkar <ayushmn29@gmail.com>
2026-07-10 20:09:38 +0530

    edge detect circuit

---
+++ edge detect circuit.v
@@ -0,0 +1,13 @@
+module top_module (
+    input clk,
+    input [7:0] in,
+    output reg [7:0] pedge
+);
+    reg [7:0] in_prev;
+    always @ (posedge clk) begin
+        in_prev<=in;
+        pedge <= in & ~in_prev; // Math: 1 & ~0 -> 1 & 1 -> 1 (Edge Detected!) for any other input combinations output will be 0 hence it either be falling edge or no change.
+    end
+   
+
+endmodule
