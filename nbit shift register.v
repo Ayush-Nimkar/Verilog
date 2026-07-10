e8e6f56a471a222b9a3d797f57b7558e46fa54d4
Ayush Nimkar <ayushmn29@gmail.com>
2026-07-10 19:46:52 +0530

    n-bit shift register

---
+++ nbit shift register.v
@@ -0,0 +1,17 @@
+module top_module (
+    input clk,
+    input w, R, E, L,
+    output reg Q
+);
+    always @ (posedge clk) begin
+        if (L) begin
+            Q = R;
+        end
+        else if (E) begin
+            Q=w;
+        end
+        else
+            Q=Q;
+    end
+              
+endmodule
