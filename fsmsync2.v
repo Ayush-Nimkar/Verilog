bb4708aa823833dd46acbeb7feea128dd8177e52
Ayush Nimkar <ayushmn29@gmail.com>
2026-07-16 18:49:29 +0530

    fsmsync2

---
+++ fsmsync2.v
@@ -0,0 +1,35 @@
+module top_module(
+    input clk,
+    input reset,    // Synchronous reset to OFF
+    input j,
+    input k,
+    output out); //  
+
+    parameter OFF=0, ON=1; 
+    reg state, next_state;
+
+    always @(*) begin
+        case(state)
+            ON:begin 
+                if (k==1) next_state = OFF;
+                else next_state = ON;
+            end
+            OFF:begin
+                if(j==1) next_state = ON;
+                else next_state = OFF;
+            end
+        endcase
+        
+    end
+
+    always @(posedge clk) begin
+        if(reset) begin
+            state <= OFF;
+        end
+        else begin
+            state <= next_state;
+        end
+    end
+    assign out = (state == ON);
+
+endmodule
